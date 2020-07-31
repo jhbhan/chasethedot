e7bed814e75fcbd4409cdad5ceef32dc442341e4
jhbhan <jhbhan@gmail.com>
2020-07-30 23:30:49 -0700

    super extra hard for angel

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const horizontalMax = $(document).innerWidth();
 const verticalMax = $(document).innerHeight();
 
+var clickcount = 0;
+var secondCounter = 0;
+var minCounter = 0;
+var xCursorPosition = 0;
+var yCursorPosition = 0;
+
+window.onload = init;
+
 $(".chase_circle").css("top", "300px");
 $(".chase_circle").css("left", "300px");
 
@@ -8,15 +16,20 @@ $(".chase_circle").mouseover(function () {
     setTimeout(moveDot, 100);
 });
 
-var clickcount = 0;
-var secondCounter = 0;
-var minCounter = 0;
+$(".bludgeon").mouseover(function () {
+    alert("YOU LOSE: you got hit by bludgeon");
+    clickcount = 0;
+    secondCounter = 0;
+    minCounter = 0;
+});
 
 $(document).click(function () {
     clickcount++;
 });
 
 var intervalID = window.setInterval(moveDot, 750);
+
+var intervalID = window.setInterval(bludgeonAttack, 500);
 var timer = window.setInterval(updateTime, 1000);
 
 function updateTime() {
@@ -53,4 +66,34 @@ function moveDot() {
     }
     $(".chase_circle").css("top", newtop + "px");
     $(".chase_circle").css("left", newleft + "px");
+}
+
+function bludgeonAttack() {
+    console.log(xCursorPosition);
+    console.log(yCursorPosition);
+
+    $(".bludgeon").css("top", yCursorPosition + "px");
+    $(".bludgeon").css("left", xCursorPosition + "px");
+}
+
+function init() {
+    if (window.Event) {
+        document.captureEvents(Event.MOUSEMOVE);
+    }
+    document.onmousemove = getCursorXY;
+}
+
+function getCursorXY(e) {
+    xCursorPosition = window.Event
+        ? e.pageX
+        : event.clientX +
+          (document.documentElement.scrollLeft
+              ? document.documentElement.scrollLeft
+              : document.body.scrollLeft);
+    yCursorPosition = window.Event
+        ? e.pageY
+        : event.clientY +
+          (document.documentElement.scrollTop
+              ? document.documentElement.scrollTop
+              : document.body.scrollTop);
 }
